f623cfd328a9b5d48e26288473a3ad3d5d46a0e1
Jiya-Jagadeesh <122512561+Jiya-Jagadeesh@users.noreply.github.com>
2023-02-11 17:49:38 +0530

    Update app(ml).py

--- Flask/app(ml).py
+++ Flask/app(ml).py
@@ -1,34 +1,49 @@
-from flask import Flask, render_template, request
-import numpy as np
 import pickle
-import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-model = pickle.load(open(r"D:\internship\Milk Grading System\Training\milk.pkl",'rb'))
+# load the model 
+naive_bayes_model = pickle.load(open(r'D:\ibm\hotel\training\naive_bayes_model.pkl', 'rb'))
+logistic_regression_model = pickle.load(open(r'D:\ibm\hotel\training\logistic_regression_model.pkl', 'rb'))
+cv_naive=pickle.load(open(r'D:\ibm\hotel\training\tranform_naive.pkl','rb'))
+cv_logistic=pickle.load(open(r'D:\ibm\hotel\training\tranform_logistic.pkl','rb'))
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def about():
+@app.route('/')
+def home():
     return render_template('home.html')
 
-@app.route("/predict")      
-def home1():
-    return render_template('predict.html')
-
-@app.route("/pred", methods=['POST','GET'])
+@app.route('/predict',methods=['POST','GET'])
 def predict():
-   x = [[x for x in request.form.values()]]
-   print(x)
-  
-   x = np.array(x)
-   print(x.shape)
-     
-     
-   print(x)
-   pred = model.predict(x)
-   print(pred[0])
-   return render_template('submit.html', prediction_text=str(pred))
+    if request.method == 'POST':
+        message = request.form['message'].lower()
+        data = [message]
+        vect_naive = cv_naive.transform(data).toarray()
+        vect_logistic = cv_logistic.transform(data).toarray()
+        # for naive bayes classifier
+        my_nb_prediction = naive_bayes_model.predict(vect_naive)
+        nb_percentage = naive_bayes_model.predict_proba(vect_naive)
+        if my_nb_prediction==1:
+            nb_percentage = nb_percentage[0][1]
+        else:
+            nb_percentage = nb_percentage[0][0]
+        # for logistic regression classifier
+        my_lg_prediction = logistic_regression_model.predict(vect_logistic)
+        lg_percentage = logistic_regression_model.predict_proba(vect_logistic)
+        if my_lg_prediction ==1:
+            lg_percentage = lg_percentage[0][1]
+        else : 
+            lg_percentage = lg_percentage[0][0]
+    return render_template('result.html',
+                message=message,
+                my_nb_prediction = my_nb_prediction,
+                nb_percentage=nb_percentage,
+                my_lg_prediction = my_lg_prediction,
+                lg_percentage=lg_percentage
+                )
 
-if __name__ == "__main__":
- app.run(debug=False)
+if __name__ == '__main__':
+    app.run(debug=True)
